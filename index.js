be3cb0aeffa38add2df5fbf1e2d15c1668aaa23b
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-07-16 17:41:42 +0600

    Redux-toolkit | redux | Store - Rakib office

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const {createStore} = require("redux")
+
+
+
 // defining constants
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
@@ -52,4 +56,18 @@ const counterReducer = (state=initialCounteState, action)=>{
 // 1.state
 // 2.dispatch action
 // 3.reducer 
-// 4.store
+// 4.store - getState(), dispatch(), subscribe()
+
+// create store
+const store = createStore(counterReducer)
+
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+
+// dispatch action
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(decrementCounter())
